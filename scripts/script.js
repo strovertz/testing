25d9cbab7e68257c6a48a80badb44c4cd892b8f9
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2023-04-02 13:11:31 -0300

    atualizando

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,9 @@ function toggleMenu() {
 // Função para ajustar a altura do iframe de acordo com a altura da janela
 function adjustIframeHeight() {
   const iframe = document.querySelector("#map-iframe");
-  iframe.style.height = window.innerHeight - 200 + "px";
+  if (iframe) {
+    iframe.style.height = window.innerHeight - 200 + "px";
+  }
 }
 
 // Chama a função para ajustar a altura do iframe inicialmente
@@ -40,7 +42,9 @@ menuLinks.forEach(function(link) {
     // Altera a URL do iframe para a URL do link clicado
     const href = this.getAttribute("href");
     const iframe = document.querySelector("#map-iframe");
-    iframe.src = href;
+    if (iframe) {
+      iframe.src = href;
+    }
   });
 });
 // Função para abrir/fechar o menu toggle
@@ -69,7 +73,8 @@ mainNavLinks.forEach(function(link) {
     toggleMenu();
   });
 });
-const imagens = document.querySelectorAll(".portifolio-item img");
+
+const imagens = document.querySelectorAll(".portfolio-item img");
 const modal = document.querySelector("#modal");
 const modalImg = document.querySelector("#modal-img");
 
@@ -83,3 +88,66 @@ imagens.forEach(function(img) {
 modal.addEventListener("click", function() {
   modal.style.display = "none";
 });
+
+// Dados do portfólio
+const portfolioData = [
+  {
+    image: 'images/portfolio-1.jpg',
+    title: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada eros quis enim lobortis.',
+    link: '#'
+  },
+  {
+    image: 'images/portfolio-2.jpg',
+    title: 'Project 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada eros quis enim lobortis.',
+    link: '#'
+  },
+  {
+    image: 'images/portfolio-3.jpg',
+    title: 'Project 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada eros quis enim lobortis.',
+    link: '#'
+  }
+];
+
+function generatePortfolioItems() {
+  const portfolioItems = document.querySelector('.portfolio-items');
+
+  portfolioData.forEach(function(item) {
+    const li = document.createElement('li');
+    li.classList.add('portfolio-item');
+    li.setAttribute('data-aos', 'fade-up');
+
+    const img = document.createElement('img');
+    img.src = item.image;
+    img.alt = 'Portfolio Item';
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('portfolio-item-overlay');
+
+    const title = document.createElement('h3');
+    title.classList.add('portfolio-item-title');
+    title.innerText = item.title;
+
+    const description = document.createElement('p');
+    description.classList.add('portfolio-item-description');
+    description.innerText = item.description;
+
+    const link = document.createElement('a');
+    link.classList.add('btn', 'btn-secondary');
+    link.href = item.link;
+    link.innerText = 'View Project';
+
+    overlay.appendChild(title);
+    overlay.appendChild(description);
+    overlay.appendChild(link);
+
+    li.appendChild(img);
+    li.appendChild(overlay);
+
+    portfolioItems.appendChild(li);
+  });
+}
+
+generatePortfolioItems();
